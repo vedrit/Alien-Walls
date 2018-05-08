f6ac3063f8a4bb271349e0801b2f673d00ce32da
TheBrain0110 <tonofpudding@gmail.com>
2018-05-08 03:29:05 +0200

    Fixed conflict with old migration file and scenarios
    Also removed all the other junk that didn't work.

--- AlienWall_0.7.0/migrations/AlienWall_0.7.0.lua
+++ AlienWall_0.7.0/migrations/AlienWall_0.7.0.lua
@@ -1,20 +1,6 @@
 for _,force in pairs(game.forces) do
-	-- Did you try turning it off and on again? :)
-	-- Pretty sure this doesn't actually do anything, since these trigger before the mod's control.lua runs, so it isn't listening for the events. Was worth a try though.
-  if force.technologies["alien-hybrid-upgrade-1"].researched then
-    force.technologies["alien-hybrid-upgrade-1"].researched = false
-	force.technologies["alien-hybrid-upgrade-1"].researched = true
-  end
-  if force.technologies["alien-hybrid-upgrade-2"].researched then
-    force.technologies["alien-hybrid-upgrade-2"].researched = false
-	force.technologies["alien-hybrid-upgrade-2"].researched = true
-  end
-  if force.technologies["alien-hybrid-upgrade-3"].researched then
-    force.technologies["alien-hybrid-upgrade-3"].researched = false
-	force.technologies["alien-hybrid-upgrade-3"].researched = true
-  end
   if force.technologies["alien-hybrid-upgrade-4"].researched then
     force.technologies["alien-hybrid-upgrade-4"].researched = false
-	force.technologies["alien-hybrid-upgrade-4"].researched = true
+	-- If they maxed out their hybrid upgrade tech, roll it back by one so they can re-research it and trigger the tier update.
   end
 end
