a3fd335df034f3d701b5ca1566c594a05cbeaf1c
Vedrit <vedrit@hotmail.com>
2018-06-02 17:31:45 -0700

    Unified all the names for walls and gates.

--- AlienWall_0.7.1/data-updates.lua
+++ AlienWall_0.7.1/data-updates.lua
@@ -7,15 +7,15 @@ local items = {}
 
 for i = 2,5 do -- could probably go real crazy and make it `for HybridHP.length` or something dynamic.
     local wall = table.deepcopy(data.raw.wall['hybridWall'])
-    wall.name='hybrid-wall-tier-'..i
+    wall.name='hybridWall-tier-'..i
     wall.max_health = HybridHP[i]
 --  wall.minable.result = wall.name -- Nope, want all wall types to drop the base item. It'll place the correct one on build.
-    wall.order = "z[hybrid-wall]"..i
+    wall.order = "z[hybridWall]"..i
     table.insert(wallTiers, wall)
     
     local item = table.deepcopy(data.raw.item['hybridized-wall'])
     item.name = wall.name
-    item.order = "z[hybrid-wall]"..i
+    item.order = "z[hybridWall]"..i
     item.place_result = wall.name
     item.hidden = true
     table.insert(items, item)
@@ -23,15 +23,15 @@ end
 
 for i = 2,5 do
     local gate = table.deepcopy(data.raw.gate['hybridGate'])
-    gate.name='hybrid-gate-tier-'..i
+    gate.name='hybridGate-tier-'..i
     gate.max_health = HybridHP[i]
 --  gate.minable.result = gate.name
-    gate.order = "z[hybrid-gate]"..i
+    gate.order = "z[hybridGate]"..i
     table.insert(gateTiers, gate)
     
     local item = table.deepcopy(data.raw.item['hybridized-gate'])
     item.name = gate.name
-    item.order = "z[hybrid-gate]"..i
+    item.order = "z[hybridGate]"..i
     item.place_result = gate.name
     item.hidden = true
     table.insert(items, item)
--- AlienWall_0.7.1/prototypes/structures/item.lua
+++ AlienWall_0.7.1/prototypes/structures/item.lua
@@ -7,7 +7,7 @@ data:extend(
 	icon_size = 32,
     flags = {"goes-to-quickbar"},
     subgroup = "structures",
-    order = "a[hybrid-wall]",
+    order = "a[hybridWall]",
     place_result = "hybridWall",
     stack_size = 50
   },
@@ -18,8 +18,8 @@ data:extend(
 	icon_size = 32,
     flags = {"goes-to-quickbar"},
     subgroup = "structures",
-    order = "b[hybrid-gate]",
-    place_result = "hybrid-gate",
+    order = "b[hybridGate]",
+    place_result = "hybridGate",
     stack_size = 50
   },
 }
