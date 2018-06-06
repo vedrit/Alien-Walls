9ba2dcc8f74601702b12f36da2b1720ccda87f88
TheBrain0110 <tonofpudding@gmail.com>
2018-06-06 13:46:15 -0400

    Restore TheBrain0110's fixes

--- AlienWall_0.7.1/migrations/AlienWall_0.7.0.lua
+++ AlienWall_0.7.1/migrations/AlienWall_0.7.0.lua
@@ -1,28 +1,6 @@
---require("variable")
---game.reload_script()
-for index,force in pairs(game.forces) do
-  if force.technologies["alien-hybridization-1"].researched then
-    --force.technologies['alien-bioengineering'].researched = true
-	force.recipes['alien-bioconstruct'].enabled = true
-	end
-	if force.technologies["alien-hybrid-upgrade-1"].researched then
-        HybridRegen = 5
-        HybridTier = 2 --Not sure global will be available for migration
-        update_walls() --Not sure this will work either
-    end      
-    if force.technologies["alien-hybrid-upgrade-2"].researched then
-        HybridRegen = 10
-        HybridTier = 3
-        update_walls()
-    end
-    if force.technologies["alien-hybrid-upgrade-3"].researched then
-        HybridRegen = 15
-        HybridTier = 4
-        update_walls()
-    end
-    if force.technologies["alien-hybrid-upgrade-4"].researched then
-        HybridRegen = 25
-        HybridTier = 5
-        update_walls()
-    end
+for _,force in pairs(game.forces) do
+  if force.technologies["alien-hybrid-upgrade-4"].researched then
+    force.technologies["alien-hybrid-upgrade-4"].researched = false
+	-- If they maxed out their hybrid upgrade tech, roll it back by one so they can re-research it and trigger the tier update.
+  end
 end
